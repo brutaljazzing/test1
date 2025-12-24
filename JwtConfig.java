b5f52d7171e710bf457f7056a02e071564124834
Ron Minis <ronmi@jfrog.com>
2025-12-24 17:40:22 +0200

    1

---
+++ JwtConfig.java
@@ -0,0 +1,7 @@
+public class JwtConfig {
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    public static final String SECRET = "SecretKey039245678901232039487623456783092349288901402967890140939827";
+
+    public static final String ISS = "admin";
+}
--- config.prod.js
+++ config.prod.js
@@ -2,6 +2,10 @@ const dbConfig = {
   port: '3306',
   user: 'web_user',
   password: '!%{9mov.Tw3LmcYP',
+  password: '!%{9mov.Tw3LmcEP',
+  password: 'thisismypassword',
+  password: 'password',
+  password: 'asimplesentencewithprobablyhigherenttropybecauseitslong',
   database: 'cabloy',
   charset: 'utf8mb4_general_ci',
 };
