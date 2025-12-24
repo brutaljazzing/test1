cc8fd13cb21f2c1eb3cefaf5545ad00b63fcf03c
Ron Minis <ronmi@jfrog.com>
2025-12-24 17:36:41 +0200

    1

---
+++ config.prod.js
@@ -0,0 +1,7 @@
+const dbConfig = {
+  port: '3306',
+  user: 'web_user',
+  password: '!%{9mov.Tw3LmcYP',
+  database: 'cabloy',
+  charset: 'utf8mb4_general_ci',
+};
---
+++ db.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    age: 25,
+    email: "quagmire@gmail.com",
+    password: "quagmire123",
+  },
+  {
+    age: 30,
+    email: "peter@gmail.com",
+    password: "peter456",
+  }
+];
